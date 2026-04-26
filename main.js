ae55f5074913f77bedb0ee181b7de232cb25d11b
1918530969@qq.com <1918530969@qq.com>
2026-04-26 13:29:10 +0800

    style(main): 修复代码格式并默认禁用webhook功能
    调整代码注释和变量声明的格式以提高可读性，同时将webhook功能默认设置为禁用状态

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ const __dirname = path.dirname(__filename);
 // 添加静态文件服务
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 8000;//服务端口
-const useAuth = false//使用授权服务
-const webhook = true//启动一对多WebHook转发服务，会把收到的消息二次转发到指定url（支持批量），适用于那些只支持原生webhook的框架（或者别的用途.?）
+const port = 8000; //服务端口
+const useAuth = false //使用授权服务
+const webhook = false //启动一对多WebHook转发服务，会把收到的消息二次转发到指定url（支持批量），适用于那些只支持原生webhook的框架（或者别的用途.?）
 const ownerSecret = process.env.OWNER_SECRET || '211217'// 主人密钥（强烈建议通过环境变量 OWNER_SECRET 设置）
 
 const authfile = `${process.cwd().replace(/\\/g, '/')}/auth.json`
